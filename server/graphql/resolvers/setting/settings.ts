80d17ea591fd7dc2878eb7e37300a3d212315bf8
leemyunggyu <jaylee@hatiolab.com>
2019-08-06 18:27:17 +0800

    v0.2.13

--- server/graphql/resolvers/setting/settings.ts
+++ server/graphql/resolvers/setting/settings.ts
@@ -3,9 +3,9 @@ import { getRepository } from 'typeorm'
 import { Setting } from '../../../entities'
 
 export const settingsResolver = {
-  async settings(_: any, params: ListParam) {
+  async settings(_: any, params: ListParam, context: any) {
     const queryBuilder = getRepository(Setting).createQueryBuilder()
-    buildQuery(queryBuilder, params)
+    buildQuery(queryBuilder, params, context)
     const [items, total] = await queryBuilder
       .leftJoinAndSelect('Setting.domain', 'Domain')
       .leftJoinAndSelect('Setting.creator', 'Creator')
