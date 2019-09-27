554f80df8875061b67cfdee39c1947bdffa718d1
Hearty Oh <heartyoh@hatiolab.com>
2019-09-27 15:59:51 +0800

    v0.2.21

--- server/graphql/resolvers/setting/update-setting.ts
+++ server/graphql/resolvers/setting/update-setting.ts
@@ -4,7 +4,7 @@ import { Setting } from '../../../entities'
 export const updateSetting = {
   async updateSetting(_: any, { name, patch }, context: any) {
     const repository = getRepository(Setting)
-    const setting = await repository.findOne({ name })
+    const setting = await repository.findOne({ domain: context.state.domain, name })
 
     return await repository.save({
       ...setting,
